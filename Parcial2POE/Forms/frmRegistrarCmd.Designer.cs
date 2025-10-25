a0b49065b713f8931be202cf2c0d0f3bb80581af
Emerson Ventura <emersonventura772@gmail.com>
2025-10-25 17:43:27 -0600

    primer commit

--- Parcial2POE/Forms/frmRegistrarCmd.Designer.cs
+++ Parcial2POE/Forms/frmRegistrarCmd.Designer.cs
@@ -119,7 +119,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(187, 30);
+            this.label5.Location = new System.Drawing.Point(255, 30);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(498, 32);
